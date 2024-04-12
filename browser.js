bec00082e68f8d76e20c300976072d52d6a380b0
Vi Nguyen <levinguyen170602@gmail.com>
2024-04-12 21:02:51 +0700

    redeploy

--- browser.js
+++ browser.js
@@ -8,10 +8,15 @@ const filePath = path.join(
 const startBrowser = async () => {
   let browser;
   try {
+    const chromePath = path.join(
+      process.cwd(), // Thư mục làm việc hiện tại trên Vercel
+      "vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe"
+    );
+
     browser = await puppeteer.launch({
       headless: true,
       //executablePath: "./chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
-      executablePath: filePath,
+      executablePath: chromePath,
       // executablePath:
       //   "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -19,7 +24,7 @@ const startBrowser = async () => {
     });
   } catch (error) {
     console.log("1234");
-    console.log("File path", filePath);
+    console.log("File path", chromePath);
     console.log("Unable to create browser: " + error);
   }
 
