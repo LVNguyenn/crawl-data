f8a23006a5d5c1651abcaf12dd2071df824ca5be
Vi Nguyen <levinguyen170602@gmail.com>
2024-04-12 20:59:10 +0700

    redeploy

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer-core");
 const path = require("path");
 const filePath = path.join(
   __dirname,
-  "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome"
+  "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe"
 );
 
 const startBrowser = async () => {
@@ -11,9 +11,9 @@ const startBrowser = async () => {
     browser = await puppeteer.launch({
       headless: true,
       //executablePath: "./chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
-      //executablePath: filePath,
-      executablePath:
-        "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
+      executablePath: filePath,
+      // executablePath:
+      //   "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
       ignoreHTTPSErrors: true,
     });
