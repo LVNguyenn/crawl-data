7ff672a83ff1dbee0f95c212e89e2f1de46e7d6f
Vi Nguyen <levinguyen170602@gmail.com>
2024-04-13 15:22:11 +0700

    redeploy

--- browser.js
+++ browser.js
@@ -4,27 +4,15 @@ const path = require("path");
 const startBrowser = async () => {
   let browser;
   try {
-    // const chromePath = path.join(
-    //   process.cwd(), // Thư mục làm việc hiện tại trên Vercel
-    //   "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe"
-    // );
     browser = await puppeteer.launch({
       headless: true,
       //executablePath: "./chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
       //executablePath: chromePath,
-      // executablePath:
-      //   "/vercel/path0/chrome/linux-116.0.5793.0/chrome-linux64/chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
+      executablePath:
+        "/vercel/.cache/puppeteer/chrome/linux-123.0.6312.122/chrome-linux64/chrome",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
       ignoreHTTPSErrors: true,
     });
-    // browser = await puppeteer.launch({
-    //   args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-    //   defaultViewport: chrome.defaultViewport,
-    //   //executablePath: await chrome.executablePath,
-    //   executablePath: "./chrome/win64-116.0.5793.0/chrome-win64/chrome.exe",
-    //   headless: true,
-    //   ignoreHTTPSErrors: true,
-    // });
   } catch (error) {
     console.log("1234");
     console.log("Unable to create browser: " + error);
