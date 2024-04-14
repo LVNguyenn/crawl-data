245cb5059878c8a4693e709b4be3d6b6ebbfb068
Vi Nguyen <levinguyen170602@gmail.com>
2024-04-14 18:41:09 +0700

    redeploy

--- index.js
+++ index.js
@@ -1,140 +1,9 @@
-const startBrowser = require("./browser");
-const scrapeController = require("./scrapeController");
-//const cron = require("node-cron");
-const path = require("path");
 const express = require("express");
-const fs = require("fs");
 const app = express();
-const filePath = path.join(__dirname, "./article.json");
 const connectDB = require("./db/connect");
 const Article = require("./models/articleModel");
-const scraper = require("./scraper");
 const axios = require("axios");
 require("dotenv").config();
-
-// cron.schedule(
-//   "*/60 * * * * *",
-//   async () => {
-//     console.log("Running crawler...");
-
-//     let browser = startBrowser();
-//     await scrapeController(browser);
-//   },
-//   {
-//     timezone: "Asia/Ho_Chi_Minh",
-//   }
-// );
-
-// async function runCrawler() {
-//   let browser = startBrowser();
-//   await scrapeController(browser);
-
-//   setTimeout(runCrawler, 60000);
-// }
-
-// runCrawler();
-
-// app.get("/api/articles/:id", (req, res) => {
-//   const articleId = req.params.id;
-
-//   fs.readFile(filePath, "utf8", (err, data) => {
-//     if (err) {
-//       console.error("Error reading file:", err);
-//       res.status(500).json({ status: "failed", msg: "Internal server error" });
-//       return;
-//     }
-
-//     try {
-//       const jsonData = JSON.parse(data);
-
-//       const article = jsonData.find((article) => article.id === articleId);
-
-//       if (!article) {
-//         res.status(404).json({ status: "failed", msg: "Article not found" });
-//       } else {
-//         res.json(article);
-//       }
-//     } catch (parseError) {
-//       console.error("Error parsing JSON:", parseError);
-//       res.status(500).json({ status: "failed", msg: "Internal server error" });
-//     }
-//   });
-// });
-
-// app.get("/api/articles", (req, res) => {
-//   fs.readFile(filePath, "utf8", (err, data) => {
-//     if (err) {
-//       console.error("Error reading file:", err);
-//       res.status(500).json({ status: "failed", msg: "Internal server error" });
-//       return;
-//     }
-
-//     try {
-//       const articlesData = JSON.parse(data);
-
-//       const page = Math.abs(parseInt(req.query.page)) || 1;
-//       const perPage = Math.abs(parseInt(req.query.perPage)) || 5;
-
-//       const startIndex = (page - 1) * perPage;
-//       const endIndex = startIndex + perPage;
-
-//       const articlesOnPage = articlesData.slice(startIndex, endIndex);
-
-//       const transformedArticles = articlesOnPage.map((article) => ({
-//         id: article.id,
-//         thumbnail: article.thumbnail,
-//         title: article.title,
-//         publish: article.meta.publish,
-//         summary: article.summary,
-//       }));
-
-//       const hasNextPage = endIndex < articlesData.length;
-
-//       res.json({
-//         article: transformedArticles,
-//         page: page,
-//         perPage: perPage,
-//         total: articlesData.length,
-//         hasNextPage: hasNextPage,
-//       });
-//     } catch (parseError) {
-//       console.error("Error parsing JSON:", parseError);
-//       res.status(500).json({ status: "failed", msg: "Internal server error" });
-//     }
-//   });
-// });
-
-// app.get("/scrape", async (req, res) => {
-//   const url = "https://lifestyle.znews.vn/oto-xe-may.html";
-//   let browser1 = await startBrowser();
-
-//   try {
-//     const browser = browser1;
-
-//     const result = await scraper(browser, url);
-
-//     if (result.length !== 0) {
-//       try {
-//         await Article.insertMany(result);
-
-//         res
-//           .status(200)
-//           .json({ message: "Added new data to MongoDB successfully." });
-//       } catch (error) {
-//         console.error("Error saving data to MongoDB:", error);
-//         res.status(500).json({ error: "Internal Server Error" });
-//       }
-//     } else {
-//       res.status(200).json({ message: "No new articles" });
-//     }
-
-//     await browser.close();
-//     console.log("Browser is closed");
-//   } catch (error) {
-//     console.log("Error in scrape route: ", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
 
 app.get("/scrape", async (req, res) => {
   const scrapeApiUrl = "https://crawl-4tjn.onrender.com/scrape";
@@ -143,7 +12,6 @@ app.get("/scrape", async (req, res) => {
     const response = await axios.get(scrapeApiUrl);
 
     if (response.status === 200) {
-      // Nếu API crawl thành công, xử lý dữ liệu trả về ở đây (nếu cần)
       res
         .status(200)
         .json({ message: "Successfully triggered external scraper" });
@@ -156,13 +24,14 @@ app.get("/scrape", async (req, res) => {
   }
 });
 
-app.get("/api/articles", async (req, res) => {
+app.get("/articles", async (req, res) => {
   try {
     const page = Math.abs(parseInt(req.query.page)) || 1;
     const perPage = Math.abs(parseInt(req.query.perPage)) || 5;
     const startIndex = (page - 1) * perPage;
 
     const articlesData = await Article.find()
+      .sort({ "meta.publish": -1 })
       .skip(startIndex)
       .limit(perPage)
       .exec();
@@ -191,7 +60,7 @@ app.get("/api/articles", async (req, res) => {
   }
 });
 
-app.get("/api/articles/:id", async (req, res) => {
+app.get("/articles/:id", async (req, res) => {
   const articleId = req.params.id;
 
   try {
